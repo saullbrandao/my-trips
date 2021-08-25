2470de44bea62334bd08bfd945f8a71a9da1d480
Saull Brandão <saull@outlook.com>
2021-08-25 14:52:35 -0300

    feat: add leaflet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,12 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>My Trips</title>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+          crossOrigin=""
+        />
         <meta
           name="description"
           content="A simple project to show my favorite sports in the world."
