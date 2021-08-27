d262319f979ec494b754642ad1c83b178b2e31b7
Saull Brandão <saull@outlook.com>
2021-08-27 14:59:04 -0300

    feat: generate dynamic pages for places

---
+++ src/pages/place/[slug].tsx
@@ -0,0 +1,51 @@
+import { GetStaticPaths, GetStaticProps } from 'next'
+import client from 'graphql/client'
+import { GetPlaceBySlugQuery, GetPlacesQuery } from 'graphql/generated/graphql'
+import { GET_PLACES, GET_PLACE_BY_SLUG } from 'graphql/queries'
+import { useRouter } from 'next/dist/client/router'
+import PlacesTemplate, { PlacesTemplateProps } from 'templates/Places'
+
+const Place = ({ place }: PlacesTemplateProps) => {
+  const router = useRouter()
+
+  if (router.isFallback) return null
+
+  return <PlacesTemplate place={place} />
+}
+
+export default Place
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { places } = await client.request<GetPlacesQuery>(GET_PLACES, {
+    first: 3
+  })
+
+  const paths = places.map(({ slug }) => ({
+    params: { slug }
+  }))
+
+  return {
+    paths,
+    fallback: true
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { place } = await client.request<GetPlaceBySlugQuery>(
+    GET_PLACE_BY_SLUG,
+    {
+      slug: params?.slug
+    }
+  )
+
+  if (!place)
+    return {
+      notFound: true
+    }
+
+  return {
+    props: {
+      place
+    }
+  }
+}
