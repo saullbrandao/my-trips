c4667e92aa3e7dfb4355c588744070ee6ee97919
Saull Brandão <saull@outlook.com>
2021-08-30 13:30:53 -0300

    feat: use mapbox tile layers

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,6 +1,10 @@
 import { useRouter } from 'next/dist/client/router'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
+const MAPBOX_API_KEY = process.env.NEXT_PUBLIC_MAPBOX_API_KEY
+const MAPBOX_USER_ID = process.env.NEXT_PUBLIC_MAPBOX_USER_ID
+const MAPBOX_STYLE_ID = process.env.NEXT_PUBLIC_MAPBOX_STYLE_ID
+
 type Place = {
   id: string
   name: string
@@ -15,19 +19,29 @@ export type MapProps = {
   places?: Place[]
 }
 
+const CustomTileLayer = () => {
+  return MAPBOX_API_KEY ? (
+    <TileLayer
+      attribution='© <a href="https://apps.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      url={`https://api.mapbox.com/styles/v1/${MAPBOX_USER_ID}/${MAPBOX_STYLE_ID}/tiles/256/{z}/{x}/{y}@2x?access_token=${MAPBOX_API_KEY}`}
+    />
+  ) : (
+    <TileLayer
+      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+  )
+}
+
 const Map = ({ places }: MapProps) => {
   const router = useRouter()
-
   return (
     <MapContainer
       center={[0, 0]}
       zoom={3}
       style={{ height: '100%', width: '100%' }}
     >
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      <CustomTileLayer />
       {places?.map(({ id, name, slug, location }) => {
         const { latitude, longitude } = location
 
